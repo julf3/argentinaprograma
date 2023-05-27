f1b490c9172ba69f1f8151729bde97c0ebd04c3d
julf3 <juliomferreiro@gmail.com>
2023-05-27 19:57:24 -0300

    Agrego el Archivo conversionGrados.js

---
+++ conversionGrados.js
@@ -0,0 +1,19 @@
+//Ejercio 3 a)
+
+var fahrenheit = 50;
+
+var celsius = (fahrenheit - 32) * (5/9);
+
+console.log(fahrenheit + " grados Fahrenheit equivalen a " + celsius + " grados Celsius.");
+
+
+//Ejercio 3 b)
+
+var fahrenheit = 75;
+
+var celsius = (fahrenheit - 32) * (5/9);
+
+console.log(fahrenheit + " grados Fahrenheit equivalen a " + celsius + " grados Celsius.");
+
+//a) 50 grados Fahrenheit equivalen a 10 grados Celsius.
+//b) 75 grados Fahrenheit equivalen a 23.88888888888889 grados Celsius.
