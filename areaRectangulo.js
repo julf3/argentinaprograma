1db403cf9d2a6a1dd313d785a04bb612954395e7
julf3 <juliomferreiro@gmail.com>
2023-05-27 19:59:12 -0300

    Practico N0 finalizado

---
+++ areaRectangulo.js
@@ -0,0 +1,24 @@
+//Ejercio 2 a)
+
+var base = 5; 
+
+var altura = 3;
+
+var area = base * altura;
+
+console.log("El área del rectángulo es: " + area);
+
+
+
+//Ejercio 2 b)
+
+var base = 12.5; 
+
+var altura = 5.3;
+
+var area = base * altura;
+
+console.log("El área del rectángulo es: " + area);
+
+//a)El área del rectángulo es: 15
+//b)El área del rectángulo es: 66.25
---
+++ promedio3.js
@@ -0,0 +1,42 @@
+//Ejercicio 4 a)
+
+var numero1 = 5;
+
+var numero2 = 7;
+
+var numero3 = 9;
+
+var promedio = (numero1 + numero2 + numero3) / 3;
+
+console.log("El promedio de los tres números es: " + promedio);
+
+
+//Ejercicio 4 b)
+
+var numero1 = -2;
+
+var numero2 = -3;
+
+var numero3 = -8;
+
+var promedio = (numero1 + numero2 + numero3) / 3;
+
+console.log("El promedio de los tres números es: " + promedio);
+
+
+//Ejercicio 4 c)
+
+var numero1 = -1.2;
+
+var numero2 = 5.3;
+
+var numero3 = -3.3;
+
+var promedio = (numero1 + numero2 + numero3) / 3;
+
+console.log("El promedio de los tres números es: " + promedio);
+
+
+//a)El promedio de los tres números es: 7
+//b)El promedio de los tres números es: -4.333333333333333
+//c)El promedio de los tres números es: 0.2666666666666666
