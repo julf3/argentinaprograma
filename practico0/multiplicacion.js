be1ba6f10189b308b0efc2300cb40270df3b4dab
julf3 <juliomferreiro@gmail.com>
2023-05-27 19:49:31 -0300

    Agrego carpeta de practico0

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,16 @@
+//Ejercio 1 b)
+
+var numero1 = 2;
+
+var numero2 = 17;
+
+var numero3 = 6;
+ 
+
+var resultado = numero1 * numero2 * numero3;
+
+
+console.log("El resultado de la multiplicación es: " + resultado);
+
+
+//El resultado de la multiplicación es: 204
---
+++ practico0/resta.js
@@ -0,0 +1,14 @@
+//Ejercio 1 a)
+
+var minuendo = 10;
+
+var sustraendo = 2;
+
+
+var resultado = minuendo - sustraendo;
+
+
+console.log("El resultado de la resta es: " + resultado);
+
+
+//El resultado de la resta es: 8
