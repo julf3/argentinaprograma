1dfb4c6021c2613c0f188340e0278879c565a44e
julf3 <juliomferreiro@gmail.com>
2023-06-27 20:35:05 -0300

    Agrego archivo lanzamiento.js

---
+++ practico3/lanzamiento.js
@@ -0,0 +1,13 @@
+//Cuenta Regresiva
+
+for (let i = 10; i >= 0; i--) 
+{if (i === 10) 
+    {console.log("Cuenta regresiva 10");} 
+    else if (i === 0) 
+    {console.log("Lanzamiento!");} 
+    else {console.log(i);}
+  }
+
+
+
+  
